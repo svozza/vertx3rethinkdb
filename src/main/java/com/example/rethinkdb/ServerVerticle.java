b0f73b3fa92f8b615ec6a26bd1cd536814f3d472
Stefano Vozza <svozza@gmail.com>
2016-03-23 21:39:34 +0000

    use router

--- src/main/java/com/example/rethinkdb/ServerVerticle.java
+++ src/main/java/com/example/rethinkdb/ServerVerticle.java
@@ -2,16 +2,26 @@ package com.example.rethinkdb;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class ServerVerticle extends AbstractVerticle {
 
     @Override
     public void start(Future<Void> fut) {
+        Router router = Router.router(vertx);
+
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response
+                    .putHeader("content-type", "text/html")
+                    .end("<h1>RethinkDb</h1>");
+        });
+
         vertx
                 .createHttpServer()
-                .requestHandler(r -> {
-                    r.response().end("<h1>RethinkDb</h1>");
-                })
+                .requestHandler(router::accept)
                 .listen(8080, result -> {
                     if (result.succeeded()) {
                         fut.complete();
